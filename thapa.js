c727d42c3dfb44b4051985962311d3ebd84f779e
vivek giri <https://github.com/vivekgiri989/user.html>
2025-09-28 15:20:52 +0530

    java commit

--- thapa.js
+++ thapa.js
@@ -1,7 +1,10 @@
 console.log("this is console");
 
-var a=10
-var b=20
-var c=a+b
+var a = 10
+var b = 20
+var c = a + b
 console.log("this is C: +c");
 console.log("this is second console");
+
+let man = "thapa singh ji ";
+let add = "vill";
